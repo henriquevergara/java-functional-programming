251cbbb15f5ccd8352862bfa3cb649c5d605db53
Henrique <henrique.vergara.hv@gmail.com>
2022-03-20 19:28:25 -0300

    Chaining functions with andThen

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -6,6 +6,10 @@ public class _Function {
 
   static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
 
+  static Function<Integer, Integer> multiplyBy10 = number -> number * 10;
+
+  static Function<Integer, Integer> incrementAndMutiplyBy10 = incrementByOneFunction.andThen(multiplyBy10);
+
   static int increment(int number) {
     return number + 1;
   }
@@ -14,5 +18,11 @@ public class _Function {
     System.out.println(increment(0));
 
     System.out.println(incrementByOneFunction.apply(1));
+
+    incrementByOneFunction.andThen(multiplyBy10).apply(1);
+
+    System.out.println(incrementByOneFunction.andThen(multiplyBy10).apply(1));
+
+    System.out.println(incrementAndMutiplyBy10.apply(1));
   }
 }
